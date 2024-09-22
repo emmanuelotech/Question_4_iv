65656ab6da63570ed7690007238034138442402e
emmanuelotech <150592072+emmanuelotech@users.noreply.github.com>
2024-09-22 12:47:26 +0300

    Create Questions_4_iv.cpp
    Asks for user input of email address

---
+++ Questions_4_iv.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    int b = 3;
+    char email [20];
+
+    cout << "Enter your email: ";
+    cin >> email;
+
+    cout << "email: " << email << endl;
+    
+    return 0;
+}
